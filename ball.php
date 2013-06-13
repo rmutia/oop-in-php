33e642d9a1b02d7b03cc5cc6f0e86d0dcffc74b2
Ratna Mutia <ratna.mutia.suci@gmail.com>
2013-06-14 06:22:13 +0700

    First class Ball

---
+++ ball.php
@@ -0,0 +1,29 @@
+<?php
+namespace OOPPHP; //namespace
+
+//class
+class Ball{ 
+	private $type; //property
+
+	//construct method
+  	public function __construct($type){ 
+    	$this->type = $type;
+	}
+
+	//method get Type
+	public function getType(){
+		return $this->type; //return
+	}
+
+	//method Rebound
+  	public function rebound(){
+		echo '<br>'.$this->type.' ball bouncing'; //print
+	}
+
+	///method Thrown
+	public function thrown(){
+		echo '<br>'.$this->type.' ball thrown';
+	}
+
+} //end class
+
